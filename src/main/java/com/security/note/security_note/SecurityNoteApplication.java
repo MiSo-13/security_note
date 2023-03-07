5796bcb926356f94a4e9f90c2b568801653e658b
MiSo <jimrayder3701@gmail.com>
2023-03-07 13:28:43 +0900

    Spring Security Init in IntelliJ Gradle
    IntelliJ Gradle을 이용해서 Spring Security Project 초기 셋팅

---
+++ src/main/java/com/security/note/security_note/SecurityNoteApplication.java
@@ -0,0 +1,13 @@
+package com.security.note.security_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/note/security_note/SecurityNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.note.security_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
